4a810fc95dc5a77b2e21dc7b2b369939ef0cac1c
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2024-09-30 14:41:41 -0300

    Hashrouter fixed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "Redux/app/store";
-import { Provider } from "react-redux";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import "./assets/styles/main.css";
-import "./assets/styles/responsive.css";
+import { store } from 'Redux/app/store'
+import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import './assets/styles/main.css'
+import './assets/styles/responsive.css'
 // import { AntUpload } from "./Practics";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
         {/* <AntUpload /> */}
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
